befbe52cb5c97c2c3d245db5e2c3b6973227ace2
Babin6139 <khatribabin39@gmail.com>
2022-08-06 14:34:19 +0545

    Creating account using wallet key and creation of nft

--- programs/hackfest/src/instructions/create_account.rs
+++ programs/hackfest/src/instructions/create_account.rs
@@ -5,14 +5,19 @@ pub fn create_account(ctx:Context<UserAccount>,name:String,phone_no:String)->Res
     let new_account=&mut ctx.accounts.user_account;
     new_account.name=name;
     new_account.phone_no=phone_no;
-
+    new_account.authority=ctx.accounts.authority.key();
     Ok(())
 }
 
 
 #[derive(Accounts)]
 pub struct UserAccount<'info>{
-    #[account(init,payer=authority,space= 8+32+32+40)]
+    #[account(
+        init,
+        payer=authority,
+        space=8 + 40 + 40 +32+32,
+        seeds=[authority.key.as_ref(),b"_profile"],
+        bump)]
     pub user_account:Account<'info,UserData>,
     #[account(mut)]
     authority:Signer<'info>,
@@ -22,5 +27,7 @@ pub struct UserAccount<'info>{
 #[account]
 pub struct UserData{
     pub name:String,
-    pub phone_no:String
+    pub phone_no:String,
+    pub land_count:u32,
+    pub authority:Pubkey
 }
--- programs/hackfest/src/instructions/mod.rs
+++ programs/hackfest/src/instructions/mod.rs
@@ -1,4 +1,5 @@
 pub mod create_account;
-
+pub mod mint_nft;
 
 pub use create_account::*;
+pub use mint_nft::*;
