9081709d820a9d1eb0ab846061237a4fd4e2d0c7
Babin6139 <khatribabin39@gmail.com>
2022-08-10 22:33:42 +0545

    Mint nft page updated

--- app/src/pages/mint-nft/index.js
+++ app/src/pages/mint-nft/index.js
@@ -4,9 +4,10 @@ function MintNft() {
   return (
     <div className='mintnft'>
         <div className='mintnft_form'>
+          <h2>Create your land NFT</h2>
             <input type="text" placeholder='Enter the name of NFT'></input>
             <input type="text" placeholder='Enter the name of NFT'></input>
-            <input type="image"></input>
+            <input type="file"></input>
             <input type="submit" value='Mint Nft'></input>
         </div>
     </div>
